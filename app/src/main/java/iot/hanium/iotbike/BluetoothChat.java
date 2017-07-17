09db9634ba0f7b8194f406b8c6c8f450c6eb828f
KyuYeol <rkswlrbduf@gmail.com>
2017-07-17 20:31:16 +0900

    Initial Commit

---
+++ app/src/main/java/iot/hanium/iotbike/BluetoothChat.java
@@ -0,0 +1,377 @@
+package iot.hanium.iotbike;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
+public class BluetoothChat extends Activity {
+
+    private static final String TAG = "BluetoothChat";
+    private static final boolean D = true;
+
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+
+    private TextView mTitle;
+    private Button mScanButton;
+    private Button mCaliButton;
+
+    private final int mMaxValue = 100;
+    private LineChart acLineChart;
+    private LineChart gyLineChart;
+    private LineDataSet axLineDataSet;
+    private LineDataSet ayLineDataSet;
+    private LineDataSet gxLineDataSet;
+    private LineDataSet gyLineDataSet;
+    private LineDataSet gzLineDataSet;
+    private LineData acLineData;
+    private LineData gyLineData;
+
+    private String axMessage = null;
+    private String ayMessage = null;
+    private String gxMessage = null;
+    private String gyMessage = null;
+    private String gzMessage = null;
+
+    private float axCal = 0;
+    private float ayCal = 0;
+
+    private String mConnectedDeviceName = null;
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private BluetoothChatService mChatService = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+        setContentView(R.layout.activity_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title);
+
+        mTitle = (TextView) findViewById(R.id.title_left_text);
+        mTitle.setText(R.string.app_name);
+        mTitle = (TextView) findViewById(R.id.title_right_text);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        acLineChart = (LineChart) findViewById(R.id.acChart);
+        gyLineChart = (LineChart) findViewById(R.id.gyChart);
+
+        acLineData = new LineData();
+        gyLineData = new LineData();
+        axLineDataSet = new LineDataSet(null, "AX");
+        ayLineDataSet = new LineDataSet(null, "AY");
+        gxLineDataSet = new LineDataSet(null, "GX");
+        gyLineDataSet = new LineDataSet(null, "GY");
+        gzLineDataSet = new LineDataSet(null, "GZ");
+
+        acLineData.addDataSet(axLineDataSet);
+        acLineData.addDataSet(ayLineDataSet);
+        gyLineData.addDataSet(gxLineDataSet);
+        gyLineData.addDataSet(gyLineDataSet);
+        gyLineData.addDataSet(gzLineDataSet);
+
+        acLineChart.setData(acLineData);
+        gyLineChart.setData(gyLineData);
+
+        setChartColor(axLineDataSet, Color.BLACK);
+        setChartColor(ayLineDataSet, Color.WHITE);
+
+        setLineChart(acLineChart);
+        setLineChart(gyLineChart);
+
+    }
+
+    private void setupChat() {
+        mScanButton = (Button) findViewById(R.id.button_scan);
+        mScanButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serverIntent = new Intent(getApplicationContext(), DeviceListActivity.class);
+                startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+            }
+        });
+
+        mCaliButton = (Button) findViewById(R.id.button_cali);
+        mCaliButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                axCal = Float.parseFloat(axMessage);
+                ayCal = Float.parseFloat(ayMessage);
+            }
+        });
+
+        mChatService = new BluetoothChatService(this, mHandler);
+    }
+
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_STATE_CHANGE:
+                    if (D) Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+                    switch (msg.arg1) {
+                        case BluetoothChatService.STATE_CONNECTED:
+                            mTitle.setText(R.string.title_connected_to);
+                            mTitle.append(mConnectedDeviceName);
+                            break;
+                        case BluetoothChatService.STATE_CONNECTING:
+                            mTitle.setText(R.string.title_connecting);
+                            break;
+                        case BluetoothChatService.STATE_LISTEN:
+                        case BluetoothChatService.STATE_NONE:
+                            mTitle.setText(R.string.title_not_connected);
+                            break;
+                    }
+                    break;
+                case MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    String writeMessage = new String(writeBuf);
+                    break;
+                case MESSAGE_READ:
+                    switch (msg.arg2) {
+                        case 0:
+                            try {
+                                byte[] readAxBuf = (byte[]) msg.obj;
+                                axMessage = new String(readAxBuf, 0, msg.arg1);
+                                if (axLineDataSet.getEntryCount() == 2)
+                                    axCal = Float.parseFloat(axMessage);
+                                if (axLineDataSet.getEntryCount() == mMaxValue) {
+                                    acLineData.removeXValue(0);
+                                    axLineDataSet.removeEntry(0);
+
+                                    for (Entry entry : axLineDataSet.getYVals()) {
+                                        entry.setXIndex(entry.getXIndex() - 1);
+                                    }
+                                }
+                                acLineData.addXValue("");
+                                acLineData.addEntry(new Entry(Float.parseFloat(axMessage) - axCal, axLineDataSet.getEntryCount()), 0);
+                            } catch (Exception e) {
+                            }
+                            break;
+                        case 1:
+                            try {
+                                byte[] readAyBuf = (byte[]) msg.obj;
+                                ayMessage = new String(readAyBuf, 0, msg.arg1);
+                                if (ayLineDataSet.getEntryCount() == 2)
+                                    ayCal = Float.parseFloat(ayMessage);
+                                if (ayLineDataSet.getEntryCount() == mMaxValue) {
+                                    ayLineDataSet.removeEntry(0);
+
+                                    for (Entry entry : ayLineDataSet.getYVals()) {
+                                        entry.setXIndex(entry.getXIndex() - 1);
+                                    }
+                                }
+                                acLineData.addEntry(new Entry(Float.parseFloat(ayMessage) - ayCal, ayLineDataSet.getEntryCount()), 1);
+                            } catch (Exception e) {
+                            }
+
+                            acLineChart.notifyDataSetChanged();
+                            acLineChart.invalidate();
+                            break;
+                        case 2:
+                            try {
+                                byte[] readgxBuf = (byte[]) msg.obj;
+                                gxMessage = new String(readgxBuf, 0, msg.arg1);
+                                if (gxLineDataSet.getEntryCount() == mMaxValue) {
+                                    gyLineData.removeXValue(0);
+                                    gxLineDataSet.removeEntry(0);
+
+                                    for (Entry entry : gxLineDataSet.getYVals()) {
+                                        entry.setXIndex(entry.getXIndex() - 1);
+                                    }
+                                }
+                                gyLineData.addXValue("");
+                                gyLineData.addEntry(new Entry(Float.parseFloat(gxMessage), gxLineDataSet.getEntryCount()), 0);
+                            } catch (Exception e) {
+                            }
+                            break;
+                        case 3:
+                            try {
+                                byte[] readgyBuf = (byte[]) msg.obj;
+                                gyMessage = new String(readgyBuf, 0, msg.arg1);
+                                if (gyLineDataSet.getEntryCount() == mMaxValue) {
+                                    gyLineDataSet.removeEntry(0);
+
+                                    for (Entry entry : gyLineDataSet.getYVals()) {
+                                        entry.setXIndex(entry.getXIndex() - 1);
+                                    }
+                                }
+                                gyLineData.addEntry(new Entry(Float.parseFloat(gyMessage), gyLineDataSet.getEntryCount()), 1);
+                            } catch (Exception e) {
+                            }
+                            break;
+                        case 4:
+                            try {
+                                byte[] readgzBuf = (byte[]) msg.obj;
+                                gzMessage = new String(readgzBuf, 0, msg.arg1);
+                                if (gzLineDataSet.getEntryCount() == mMaxValue) {
+                                    gzLineDataSet.removeEntry(0);
+
+                                    for (Entry entry : gzLineDataSet.getYVals()) {
+                                        entry.setXIndex(entry.getXIndex() - 1);
+                                    }
+                                }
+                                gyLineData.addEntry(new Entry(Float.parseFloat(gzMessage), gzLineDataSet.getEntryCount()), 2);
+                            } catch (Exception e) {
+                            }
+
+                            setChartColor(gxLineDataSet, Color.RED);
+                            setChartColor(gyLineDataSet, Color.GREEN);
+                            setChartColor(gzLineDataSet, Color.BLUE);
+
+                            Log.d("TAG", gyLineDataSet.getEntryCount() + "");
+
+                            gyLineChart.notifyDataSetChanged();
+                            gyLineChart.invalidate();
+
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case MESSAGE_DEVICE_NAME:
+                    mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                    Toast.makeText(getApplicationContext(), "Connected to "
+                            + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_TOAST:
+                    Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+    private void setLineChart(LineChart lineChart) {
+
+        lineChart.setTouchEnabled(true);
+
+        lineChart.setDragEnabled(true);
+        lineChart.setScaleEnabled(true);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setBackgroundColor(Color.GRAY);
+
+        lineChart.setPinchZoom(true);
+
+        Legend L = lineChart.getLegend();
+        L.setForm(Legend.LegendForm.LINE);
+
+        XAxis xl = lineChart.getXAxis();
+        xl.setDrawGridLines(false);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis leftAxis = lineChart.getAxisLeft();
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setAxisMaxValue(20000f);
+        leftAxis.setAxisMinValue(-20000f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = lineChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+    }
+
+    private void setChartColor(LineDataSet lineDataSet, int Color) {
+        lineDataSet.setColor(Color);
+        lineDataSet.setCircleColor(Color);
+        lineDataSet.setDrawCubic(true);
+        lineDataSet.setDrawCircles(false);
+        lineDataSet.setDrawValues(false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        } else {
+            if (mChatService == null) setupChat();
+        }
+    }
+
+    @Override
+    public synchronized void onResume() {
+        super.onResume();
+        if (mChatService != null) {
+            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+                mChatService.start();
+            }
+        }
+    }
+
+    @Override
+    public synchronized void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mChatService != null) mChatService.stop();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE:
+                if (resultCode == Activity.RESULT_OK) {
+                    String address = data.getExtras()
+                            .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+                    BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+                    mChatService.connect(device);
+                }
+                break;
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK) {
+                    setupChat();
+                } else {
+                    Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+}
---
+++ app/src/main/java/iot/hanium/iotbike/BluetoothChatService.java
@@ -0,0 +1,343 @@
+package iot.hanium.iotbike;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class BluetoothChatService {
+    private static final String TAG = "BluetoothChatService";
+    private static final boolean D = true;
+
+    private static final String NAME = "BluetoothChat";
+
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    private final BluetoothAdapter mAdapter;
+    private final Handler mHandler;
+    private AcceptThread mAcceptThread;
+    private ConnectThread mConnectThread;
+    private ConnectedThread mConnectedThread;
+    private int mState;
+
+    public static final int STATE_NONE = 0;
+    public static final int STATE_LISTEN = 1;
+    public static final int STATE_CONNECTING = 2;
+    public static final int STATE_CONNECTED = 3;
+
+    public BluetoothChatService(Context context, Handler handler) {
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+        mState = STATE_NONE;
+        mHandler = handler;
+    }
+
+    private synchronized void setState(int state) {
+        mState = state;
+        mHandler.obtainMessage(BluetoothChat.MESSAGE_STATE_CHANGE, state, -1).sendToTarget();
+    }
+
+    public synchronized int getState() {
+        return mState;
+    }
+
+    public synchronized void start() {
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        if (mAcceptThread == null) {
+            mAcceptThread = new AcceptThread();
+            mAcceptThread.start();
+        }
+        setState(STATE_LISTEN);
+    }
+
+    public synchronized void connect(BluetoothDevice device) {
+        if (mState == STATE_CONNECTING) {
+            if (mConnectThread != null) {
+                mConnectThread.cancel();
+                mConnectThread = null;
+            }
+        }
+
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        mConnectThread = new ConnectThread(device);
+        mConnectThread.start();
+        setState(STATE_CONNECTING);
+    }
+
+    public synchronized void connected(BluetoothSocket socket, BluetoothDevice device) {
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        if (mAcceptThread != null) {
+            mAcceptThread.cancel();
+            mAcceptThread = null;
+        }
+
+        mConnectedThread = new ConnectedThread(socket);
+        mConnectedThread.start();
+
+        Message msg = mHandler.obtainMessage(BluetoothChat.MESSAGE_DEVICE_NAME);
+        Bundle bundle = new Bundle();
+        bundle.putString(BluetoothChat.DEVICE_NAME, device.getName());
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+
+        setState(STATE_CONNECTED);
+    }
+
+    public synchronized void stop() {
+        if (D) Log.d(TAG, "stop");
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+        if (mAcceptThread != null) {
+            mAcceptThread.cancel();
+            mAcceptThread = null;
+        }
+        setState(STATE_NONE);
+    }
+
+    public void write(byte[] out) {
+        ConnectedThread r;
+        synchronized (this) {
+            if (mState != STATE_CONNECTED) return;
+            r = mConnectedThread;
+        }
+        r.write(out);
+    }
+
+    private void connectionFailed() {
+        setState(STATE_LISTEN);
+
+        Message msg = mHandler.obtainMessage(BluetoothChat.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(BluetoothChat.TOAST, "Unable to connect device");
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+    }
+
+    private void connectionLost() {
+        setState(STATE_LISTEN);
+
+        Message msg = mHandler.obtainMessage(BluetoothChat.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(BluetoothChat.TOAST, "Device connection was lost");
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+    }
+
+    private class AcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread() {
+            BluetoothServerSocket tmp = null;
+
+            try {
+                tmp = mAdapter.listenUsingRfcommWithServiceRecord(NAME, MY_UUID);
+            } catch (IOException e) {
+            }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            setName("AcceptThread");
+            BluetoothSocket socket = null;
+
+            while (mState != STATE_CONNECTED) {
+                try {
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+
+                if (socket != null) {
+                    synchronized (BluetoothChatService.this) {
+                        switch (mState) {
+                            case STATE_LISTEN:
+                            case STATE_CONNECTING:
+                                connected(socket, socket.getRemoteDevice());
+                                break;
+                            case STATE_NONE:
+                            case STATE_CONNECTED:
+                                try {
+                                    socket.close();
+                                } catch (IOException e) {
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            mmDevice = device;
+            BluetoothSocket tmp = null;
+
+            try {
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            setName("ConnectThread");
+
+            mAdapter.cancelDiscovery();
+
+            try {
+                mmSocket.connect();
+            } catch (IOException e) {
+                connectionFailed();
+                try {
+                    mmSocket.close();
+                } catch (IOException e2) {
+                }
+                BluetoothChatService.this.start();
+                return;
+            }
+
+            synchronized (BluetoothChatService.this) {
+                mConnectThread = null;
+            }
+
+            connected(mmSocket, mmDevice);
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            int bytes;
+            int flag = 0;
+
+            while (true) {
+                try {
+                    byte[] axBuffer = new byte[16];
+                    byte[] ayBuffer = new byte[16];
+                    byte[] gxBuffer = new byte[16];
+                    byte[] gyBuffer = new byte[16];
+                    byte[] gzBuffer = new byte[16];
+
+                    int axStart = 0;
+                    int ayStart = 0;
+                    int gxStart = 0;
+                    int gyStart = 0;
+                    int gzStart = 0;
+
+                    InputStreamReader reader = new InputStreamReader(mmInStream);
+                    flag = 0;
+                    while ((bytes = reader.read()) != 10 && bytes != 13 && bytes != 0) {
+                        if(bytes == 9) { flag++; continue; }
+                        switch (flag) {
+                            case 0: axBuffer[axStart++] = (byte) bytes; break;
+                            case 1: ayBuffer[ayStart++] = (byte) bytes; break;
+                            case 2: gxBuffer[gxStart++] = (byte) bytes; break;
+                            case 3: gyBuffer[gyStart++] = (byte) bytes; break;
+                            case 4: gzBuffer[gzStart++] = (byte) bytes; break;
+                            default: break;
+                        }
+                    }
+                    mHandler.obtainMessage(BluetoothChat.MESSAGE_READ, axStart, 0, axBuffer).sendToTarget();
+                    mHandler.obtainMessage(BluetoothChat.MESSAGE_READ, ayStart, 1, ayBuffer).sendToTarget();
+                    mHandler.obtainMessage(BluetoothChat.MESSAGE_READ, gxStart, 2, gxBuffer).sendToTarget();
+                    mHandler.obtainMessage(BluetoothChat.MESSAGE_READ, gyStart, 3, gyBuffer).sendToTarget();
+                    mHandler.obtainMessage(BluetoothChat.MESSAGE_READ, gzStart, 4, gzBuffer).sendToTarget();
+                } catch (IOException e) {
+                    connectionLost();
+                    break;
+                }
+            }
+        }
+
+        public void write(byte[] buffer) {
+            try {
+                mmOutStream.write(buffer);
+
+                mHandler.obtainMessage(BluetoothChat.MESSAGE_WRITE, -1, -1, buffer)
+                        .sendToTarget();
+            } catch (IOException e) {
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+}
